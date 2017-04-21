625f150d385287dd94da16bdc12a404cada26f3a
denispeplin <denis.peplin@gmail.com>
2017-04-21 23:25:19 +0300

    Add call for README API (copypaste from pull request).

--- lib/tentacat/contents.ex
+++ lib/tentacat/contents.ex
@@ -2,6 +2,23 @@ defmodule Tentacat.Contents do
   import Tentacat
   alias Tentacat.Client
 
+  # copypaste from https://github.com/edgurgel/tentacat/pull/95/files
+  # TODO: add tests
+  @doc """
+  Get the contents of a file or directory in a repository.
+
+  ## Example
+
+      Tentacat.Contents.readme "elixir-lang", "elixir", "ref-name"
+      Tentacat.Contents.readme "elixir-lang", "elixir", "ref-name", client
+
+  More info at: https://developer.github.com/v3/repos/contents/#get-the-readme
+  """
+  @spec readme(binary, binary, binary, Client.t) :: Tentacat.response
+  def readme(owner, repo, ref, client \\ %Client{}) do
+    get "repos/#{owner}/#{repo}/readme", client, [{:ref, ref}]
+  end
+
   @doc """
   Get the contents of a file or directory in a repository.
 
